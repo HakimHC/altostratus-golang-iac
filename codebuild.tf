6e8644db6e92406fdf140f5f669ad8f8925b0de6
HakimHC <ahmedh90111@gmail.com>
2024-05-26 20:57:18 +0200

    Added basic codebuild project with codepipeline input

---
+++ codebuild.tf
@@ -0,0 +1,24 @@
+resource "aws_codebuild_project" "main" {
+  name           = "codebuild_project"
+  description    = "CodeBuild project that builds the Go API docker image and pushes it to ECR"
+  build_timeout  = 10
+
+  service_role = module.codebuild_role.iam_role_arn
+
+  artifacts {
+    type = "CODEPIPELINE"
+  }
+
+  environment {
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "aws/codebuild/amazonlinux2-x86_64-standard:4.0"
+    type                        = "LINUX_CONTAINER"
+    image_pull_credentials_type = "CODEBUILD"
+  }
+
+  source {
+    type            = "CODEPIPELINE"
+  }
+
+  tags = local.tags
+}
