b86e393668efdbac1427a0ecd7b267290ca79008
HakimHC <ahmedh90111@gmail.com>
2024-05-26 20:56:58 +0200

    Code Pipeline with 2 stages (source, build)

---
+++ codepipeline.tf
@@ -0,0 +1,48 @@
+resource "aws_codepipeline" "codepipeline" {
+  # TODO: change name
+  name     = "tf-test-pipeline"
+  role_arn = module.codepipeline_role.iam_role_arn
+
+  artifact_store {
+    location = aws_s3_bucket.codepipeline_bucket.bucket
+    type     = "S3"
+  }
+
+  stage {
+    name = "Source"
+
+    action {
+      name             = "Source"
+      category         = "Source"
+      owner            = "AWS"
+      provider         = "CodeStarSourceConnection"
+      version          = "1"
+      output_artifacts = ["source_output"]
+
+      configuration = {
+        ConnectionArn    = aws_codestarconnections_connection.github.arn
+        FullRepositoryId = "HakimHC/altostratus-golang-api"
+        BranchName       = "main"
+      }
+    }
+  }
+
+  stage {
+    name = "Build"
+
+    action {
+      name             = "Build"
+      category         = "Build"
+      owner            = "AWS"
+      provider         = "CodeBuild"
+      input_artifacts  = ["source_output"]
+      output_artifacts = ["build_output"]
+      version          = "1"
+
+      configuration = {
+        ProjectName = aws_codebuild_project.main.name
+      }
+    }
+  }
+
+}
